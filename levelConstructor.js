8173b66eda224da9e3b5775d95ba9d949ecca1aa
Will Bjorn <willbjorn@gmail.com>
2017-08-02 18:04:25 +0000

    Changed to canvas drawing function

--- levelConstructor.js
+++ levelConstructor.js
@@ -179,6 +179,23 @@ var jumpSpeed = 17;
 var arrows = trackKeys(arrowCodes);
 // var lives = 3;
 
+var playerSprites = document.createElement("img");
+playerSprites.src = "img/player.png";
+var playerXOverlap = 4;
+
+// .__           .__                              
+// |  |__   ____ |  | ______   ___________  ______
+// |  |  \_/ __ \|  | \____ \_/ __ \_  __ \/  ___/
+// |   Y  \  ___/|  |_|  |_> >  ___/|  | \/\___ \ 
+// |___|  /\___  >____/   __/ \___  >__|  /____  >
+//      \/     \/     |__|        \/           \/ 
+
+function flipHorizontally(context, around){
+  context.translate(around, 0);
+  context.scale(-1, 1);
+  context.translate(-around, 0);
+};
+
 
 
         
@@ -424,6 +441,7 @@ CanvasDisplay.prototype.clear = function(){
     this.canvas.parentNode.removeChild(this.canvas);
 };
 
+
 CanvasDisplay.prototype.drawFrame = function(step){
     this.animationTime += step;
     this.updateViewport();
@@ -432,6 +450,100 @@ CanvasDisplay.prototype.drawFrame = function(step){
     this.drawActors();
 }
 
+CanvasDisplay.prototype.updateViewport = function(){
+    var view = this.viewport, margin = view.width / 3;
+    var player = this.level.player;
+    var center = player.pos.plus(player.size.times(0.5));
+    
+    if (center.x < view.left + margin)
+        view.left = Math.max(center.x - margin, 0);
+    else if (center.x > view.left + view.width - margin)
+        view.left = Math.min(center.x + margin - view.width, this.level.width - view.width);
+        
+    if (center.y < view.top + margin)
+        view.top = Math.max(center.y - margin, 0);
+    else if (center.y > view.top + view.height - margin)
+        view.top = Math.min(center.y + margin - view.height, this.level.height - view.height);
+}
+
+CanvasDisplay.prototype.clearDisplay = function(){
+    if (this.level.status == "won")
+        this.cx.fillStyle = "rgba(68, 191, 255)";
+    else if (this.level.status == "lost")
+        this.cx.fillStyle = "rgba(44, 136, 214)";
+    else 
+        this.cx.fillStyle = "rgba(52, 166, 251)";
+    this.cx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+};
+
+var otherSprites = document.createElement("img");
+otherSprites.src = "img/sprites.png";
+
+CanvasDisplay.prototype.drawBackground = function(){
+  var view = this.viewport;
+  var xStart = Math.floor(view.left);
+  var xEnd = Math.ceil(view.left + view.width);
+  var yStart = Math.floor(view.top);
+  var yEnd = Math.ceil(view.top + view.height);
+  
+  for (var y = yStart; y < yEnd; y++){
+      
+      for (var x = xStart; x < xEnd; x++){
+          
+        var tile = this.level.grid[y][x];
+        if (tile == null) continue;
+        var screenX = (x - view.left) * scale;
+        var screenY = (y - view.top) * scale;
+        var tileX = tile == "lava" ? scale : 0;
+        this.cx.drawImage(otherSprites, tileX, 0, scale, scale, screenX, screenY, scale, scale);
+        
+      }
+  }
+};
+
+CanvasDisplay.prototype.drawPlayer = function(x, y, width, height){
+    var sprite = 8, player = this.level.player;
+    width += playerXOverlap * 2;
+    x -= playerXOverlap;
+    
+    if (player.speed != 0)
+        this.flipPlayer = player.speed.x < 0;
+        
+    if (player.speed.y != 0) 
+        sprite = 9;
+    else if (player.speed.x != 0)
+        sprite = Math.floor(this.animationTime * 12) % 8;
+    
+    this.cx.save();
+    if (this.flipPlayer)
+        flipHorizontally(this.cx, x + width / 2);
+        
+    this.cx.drawImage(playerSprites,
+                      sprite * width, 0, width, height,
+                      x,              y, width, height);
+    this.cx.restore();
+}
+
+
+CanvasDisplay.prototype.drawActors = function(){
+    this.level.actors.forEach(function(actor){
+        var width = actor.size.x * scale;
+        var height = actor.size.y * scale;
+        var x = (actor.pos.x - this.viewport.left) * scale;
+        var y = (actor.pos.y - this.viewport.top) * scale;
+        
+        if (actor.type == "player"){
+            this.drawPlayer(x, y, width, height);
+        } else {
+            var tileX = (actor.type == "coin" ? 2 : 1) * scale;
+            this.cx.drawImage(otherSprites,
+                              tileX, 0, width, height,
+                              x,     y, width, height);
+        }
+    }, this);
+}
+
+/////////////////////////////////////BEGIN WORK HERE PAGE 293/4
 
 // VECTOR CONTSTRUCTOR
 
@@ -620,20 +732,19 @@ Level.prototype.playerTouched = function(type, actor) {
 //      \/    \/\/           \/            \/                \/ 
 
 
+
 function trackKeys(codes) {
-    var pressed = Object.create(null);
-    
-    function handler(event) {
-        if (codes.hasOwnProperty(event.keyCode)){
-            var down = event.type == "keydown";
-            pressed[codes[event.keyCode]] = down;
-            event.preventDefault();
-            // console.log(event.keyCode, "pressed");
-        }
+  var pressed = Object.create(null);
+  function handler(event) {
+    if (codes.hasOwnProperty(event.keyCode)) {
+      var down = event.type == "keydown";
+      pressed[codes[event.keyCode]] = down;
+      event.preventDefault();
     }
-    addEventListener("keydown", handler);
-    addEventListener("keyup", handler);
-    return pressed;
+  }
+  addEventListener("keydown", handler);
+  addEventListener("keyup", handler);
+  return pressed;
 }
 
 
@@ -707,5 +818,5 @@ startLevel(0, 3);
 
 
 
-runGame(GAME_LEVELS, DOMDisplay);
+runGame(GAME_LEVELS, CanvasDisplay);
 
